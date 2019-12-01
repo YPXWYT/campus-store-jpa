7727346114c89489c411b1d1883aee0e4a0b4f29
YPXWYT <2568824264@qq.com>
2019-12-01 14:50:41 +0800

    :sparkles:添加状态1003（未校方认证）

--- src/main/java/com/tna/campus_store/beans/StatusEnum.java
+++ src/main/java/com/tna/campus_store/beans/StatusEnum.java
@@ -1,7 +1,9 @@
 package com.tna.campus_store.beans;
 
 public enum StatusEnum {
-    HINT(2001), AUTH_DUE(2002), UNAUTHORIZED(2001);
+    HINT(2001), AUTH_DUE(2002),
+    UNAUTHORIZED(2001),SUCCESS(2000),
+    UNIDENTIFICATION(1003);
 
     private Integer code;
 
